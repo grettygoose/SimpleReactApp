bde90b714c7d0a59fd340793bca5d8669cd8ad04
grettygoose <gheinric@fredhutch.org>
2018-10-03 11:03:49 -0700

    files

---
+++ robofriends/src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [{
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'test@bldha.com'
+    },
+    {
+        id: 2,
+        name: 'Daniel Krenn',
+        username: 'Fish',
+        email: 'april@bldha.com'
+    },
+    {
+        id: 3,
+        name: 'Gretchen Krenn',
+        username: 'Cake',
+        email: 'Krenn@bldha.com'
+    },
+    {
+        id: 4,
+        name: 'Richard Krenn',
+        username: 'lake',
+        email: 'Krenn@bldha.com'
+    },
+    {
+        id: 5,
+        name: 'Tom Krenn',
+        username: 'lake',
+        email: 'Krenn@bldha.com'
+    },
+    {
+        id: 6,
+        name: 'Laurie Krenn',
+        username: 'lake',
+        email: 'Krenn@bldha.com'
+    }
+
+
+]
